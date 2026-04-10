6ae5a522b9ca9d84349a7d21af4c05f833ebe33d
sakshi07bhosale <sakshibhosale749@gmail.com>
2026-04-10 11:10:44 +0530

    Type casting learn

---
+++ Day1/typecasting.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){ 
+    char grade = 'A';
+    int value = grade; // Implicit typecasting from char to int and print ASCII value
+    cout<<"The value of grade is: "<<value<<endl;
+    return 0;
+
+}
