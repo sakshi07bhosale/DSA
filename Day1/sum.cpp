cc72a00c9ceb368eef916c0816f8ff838ed3707c
sakshi07bhosale <sakshibhosale749@gmail.com>
2026-04-10 11:43:05 +0530

    sum of two numbers

---
+++ Day1/sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter a: ";
+    cin>>a;
+    cout<<"Enter b:";
+    cin>>b;
+    int sum = a+b;
+    cout<<"The sum of "<<a<<" and "<<b<<" is: "<<sum<<endl;
+    return 0;
+}
