e590db49a603a278851a8508a59d61533fcc25be
sakshi07bhosale <sakshibhosale749@gmail.com>
2026-04-10 11:03:12 +0530

    variables learn

---
+++ Day1/variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a=10;
+    float b=3.14;
+    char c='A';
+    bool d=true;
+
+    cout<<"Integer: "<<a<<endl;
+    cout<<"Float: "<<b<<endl;
+    cout<<"Character: "<<c<<endl;
+    cout<<"Boolean: "<<d<<endl;
+
+    return 0;
+}
