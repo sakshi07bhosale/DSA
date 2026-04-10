cf4099aa53174e4e28c32ec73bedf8823ba0a526
sakshi07bhosale <sakshibhosale749@gmail.com>
2026-04-10 11:16:14 +0530

    Inpute from the user

---
+++ Day1/input.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    double price;
+    cout<<"Enter the price:";
+    cin>>price;
+    cout<<"The price is:" <<price <<endl;
+    return 0;
+}
