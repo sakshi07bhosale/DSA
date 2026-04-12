fbba5dfe9f66d2ac2c7aaf2938f4575474452764
sakshi07bhosale <sakshibhosale749@gmail.com>
2026-04-12 13:01:57 +0530

    user entered n odd num print

--- Day2/loops/odd.cpp
+++ Day2/loops/odd.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int n =50;
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
     for(int i=1; i<=n; i++){
         if(i%2!=0){
             cout<<i<<" ";
