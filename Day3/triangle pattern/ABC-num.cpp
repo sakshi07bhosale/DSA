7125c3e29bdc0d578eef1799925edded802d47d8
sakshi07bhosale <sakshibhosale749@gmail.com>
2026-04-13 22:18:03 +0530

    seq alphabeat triangle pattern printting

---
+++ Day3/triangle pattern/ABC-num.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n = 5;
+    for(int i=0;i<n;i++){
+        char ch = 'A';
+        for(int j=0;j<i+1;j++){
+            cout<<ch;
+            ch++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+// output
+// A
+// AB
+// ABC
+// ABCD
+// ABCDE
