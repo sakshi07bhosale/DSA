e99f4223cc9e14d98a8f94662adde66d45f064d5
sakshi07bhosale <sakshibhosale749@gmail.com>
2026-04-13 22:22:58 +0530

    reverse num printing

---
+++ Day3/triangle pattern/reverse.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n = 5;
+
+    for(int i=n; i>0;i--){
+        cout<<i;
+    }
+    cout<<endl;
+    
+    return 0;
+}
+
+// output
+// 54321
