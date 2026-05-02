3fc8fcf96d4845d7eb718fa249fb836db37147d2
sakshi07bhosale <sakshibhosale749@gmail.com>
2026-05-02 19:45:39 +0530

    print the size of the array

---
+++ Day7/Arrays/loops.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int marks[5]={99,100,54,36,88};
+    int sz=sizeof(marks);
+    cout<<sz<<endl; // it prints that`s why int size is 4 
+    return 0;
+}
