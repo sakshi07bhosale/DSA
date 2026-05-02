73e129fc211d6b8873edf6c36ce561d7ae00ba38
sakshi07bhosale <sakshibhosale749@gmail.com>
2026-05-02 20:36:47 +0530

    pass by reference example

---
+++ Day7/Arrays/pass_by_reference.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+void changeArr(int arr[],int size){
+    cout<<"in function \n";
+    for(int i=0;i<size;i++){
+        arr[i]=2*arr[i];
+    }
+}
+
+int main(){
+    int arr[]={1,2,3,4,5,6};
+
+    changeArr(arr,6);
+
+    cout<<"in main \n";
+    for(int i=0;i<6;i++){
+        cout<<arr[i]<<endl;
+    }
+    cout<<endl;
+
+    return 0;
+}
