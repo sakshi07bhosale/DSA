1a64e5010b2e1189965e4e4970c6838682f1f320
sakshi07bhosale <sakshibhosale749@gmail.com>
2026-05-02 19:49:00 +0530

    print the arrays element one by one

--- Day7/Arrays/loops.cpp
+++ Day7/Arrays/loops.cpp
@@ -3,7 +3,14 @@ using namespace std;
 
 int main(){
     int marks[5]={99,100,54,36,88};
+    
     int sz=sizeof(marks);
-    cout<<sz<<endl; // it prints that`s why int size is 4 
+    cout<<sz<<endl; // it prints 20 that`s why int size is 4 
+
+    int size = 5;
+    for (int i=0;i<size;i++){
+        cout<<marks[i]<<endl;
+    }
+
     return 0;
 }
